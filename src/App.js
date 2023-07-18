f3f990b7b7d801b7378b390602b71b404c3be266
becem <becem.1897@hotmail.com>
2023-07-18 19:21:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlayersList from './Components/PlayersList';
+import "../src/Components/Player.css";
+ 
+function App() {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/players.js
@@ -0,0 +1,41 @@
+const players = [
+    {
+      name: 'Pavel Nedved',
+      team: 'F.C.Juventus',
+      nationality: 'Tchéque',
+      jerseyNumber: 10,
+      age: 49,
+      imageURL: 'https://i.pinimg.com/originals/7d/d8/61/7dd861892a0285f986d256bc0c459cf4.jpg',
+    },
+    // Add more player objects here
+    {
+        name: 'Del Piero',
+        team: 'F.C.Juventus',
+        nationality: 'Italy',
+        jerseyNumber: 10,
+        age: 44,
+        imageURL: 'https://th.bing.com/th/id/OIP.zTbqNJzNeo0NFbk5dOkrFAHaF8?pid=ImgDet&rs=1',
+      },
+      // Add more player objects here
+      {
+        name: 'Cristiano',
+        team: 'Al-NAssr',
+        nationality: 'Portugal',
+        jerseyNumber: 7,
+        age: 39,
+        imageURL: 'https://th.bing.com/th/id/R.55e55db9f1931f866967076f643be0f2?rik=Qi4KXrb7j3ShPw&pid=ImgRaw&r=0',
+      },
+      // Add more player objects here
+      {
+        name: 'Claudio Marchisio ',
+        team: 'Juventus',
+        nationality: 'Italy',
+        jerseyNumber: 8,
+        age: 25,
+        imageURL: 'https://th.bing.com/th/id/OIP.RMLKoljOzjZSdKgAoGVMagHaHa?pid=ImgDet&rs=1',
+      },
+      // Add more player objects here
+  ];
+  
+  export default players;
+  
